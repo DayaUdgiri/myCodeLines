1131e7e22a1f5e747d293a7e15afb44f3fbabc59
DayaUdgiri <78370748+DayaUdgiri@users.noreply.github.com>
2024-05-19 22:25:04 +0530

    Create myCodeLines.py

---
+++ myCodeLines.py
@@ -0,0 +1,7 @@
+# This is to save my code lines for quick reference
+
+# Create the Python Env without conda
+python -m ven <fodler path of env>\<env_name>
+
+# Activate python envrironment without conda
+<fodler path of env>\<env_name>\Scripts\activate.bat
